679b0bf43605ee4718987a401c013efe5cb228c7
nano <nanonicholson@gmail.com>
2020-05-05 09:34:27 +0600

    Refactor

--- src/graphics/font.rs
+++ src/graphics/font.rs
@@ -1,5 +1,8 @@
 use super::{
-    super::common::Vec2D,
+    super::common::{
+        Vec2D,
+        Rect,
+    },
     pages::Pages,
     Texture,
 };
@@ -8,6 +11,7 @@ use super::{
 pub struct Font {
     pub atlas_size: Vec2D<u32>,
     pub chars_on_page: u32,
+    pub char_size: Vec2D<u32>,
     pub st_char: Vec2D<f32>,
     pub indent: u32,
     pub pages: Pages<Texture>,
@@ -20,16 +24,47 @@ impl Font {
     {
         let atlas_size = atlas_size.into();
 
+        let (width, height) = pages.first().size().into_inner();
+        let char_width = width / atlas_size.x;
+        let char_height = height / atlas_size.y;
+
         Font {
             atlas_size,
             chars_on_page: atlas_size.x * atlas_size.y,
+            char_size: Vec2D::new(char_width, char_height),
             st_char: Vec2D::new(1.0 / atlas_size.x as f32, 1.0 / atlas_size.y as f32),
             indent,
             pages,
         }
     }
 
-    pub fn page_size(&self) -> Vec2D<u32> { self.pages.first().size() }
+    pub fn text_size(&self, text: &str) -> Vec2D<u32> {
+        match text.chars().count() {
+            0 => Vec2D::new(0, self.char_size.y),
+            1 => self.char_size,
+            l => Vec2D::new(
+                self.char_size.x + (l as u32 - 1) * (self.indent as u32 + self.char_size.x),
+                self.char_size.y,
+            ),
+        }
+    }
+
+    pub fn char_rect(&self, char_num: u32, pos: Vec2D<f32>) -> Rect<f32> {
+        let x_step = (char_num * (self.char_size.x + self.indent)) as f32;
+        Rect::new((pos.x + x_step, pos.y), self.char_size.cast::<f32>())
+    }
+
+    pub fn st_rect(&self, code: u32) -> Rect<f32> {
+        let code_at_page = code % self.chars_on_page;
+        let s = (code_at_page % self.atlas_size.x) as f32 * self.st_char.x;
+        let t = (code_at_page / self.atlas_size.x) as f32 * self.st_char.y;
+        Rect::new((s, t), (self.st_char.x, self.st_char.y))
+    }
+
+    pub fn page(&self, code: u32) -> Option<&Texture> {
+        let page_code = code / self.chars_on_page;
+        self.pages.get(page_code as usize)
+    }
 }
 
 impl Default for Font {
--- src/graphics/font_render.rs
+++ src/graphics/font_render.rs
@@ -1,8 +1,5 @@
 use super::{
-    super::common::{
-        Rect,
-        Vec2D,
-    },
+    super::common::Vec2D,
     Render,
     UsedShader,
     font::Font,
@@ -10,54 +7,28 @@ use super::{
 
 #[derive(Debug)]
 pub struct FontRender {
-    char_size: Vec2D<u32>,
     font: Font,
 }
 
 impl FontRender {
     pub fn new() -> Self {
         let font = Font::default();
-
-        let (width, height) = font.page_size().into_inner();
-        let char_width = width / font.atlas_size.x;
-        let char_height = height / font.atlas_size.y;
-
-        FontRender {
-            char_size: Vec2D::new(char_width, char_height),
-            font,
-        }
+        FontRender { font }
     }
 
     pub fn print(&self, render: &mut Render, text: &str, pos: Vec2D<f32>) {
-        for (i, code) in text.chars().map(|c| c as u32).enumerate() {
-            let font = &self.font;
-
-            let x_step = (i as u32 * (self.char_size.x + font.indent)) as f32;
-            let char = Rect::new((pos.x + x_step, pos.y), self.char_size.cast::<f32>());
-
-            let code_at_page = code % font.chars_on_page;
-            let s = (code_at_page % font.atlas_size.x) as f32 * font.st_char.x;
-            let t = (code_at_page / font.atlas_size.x) as f32 * font.st_char.y;
-            let st = Rect::new((s, t), (font.st_char.x, font.st_char.y));
-
-            let page_code = code / font.chars_on_page;
-            match font.pages.get(page_code as usize) {
+        for (n, code) in text.chars().map(|c| c as u32).enumerate() {
+            match self.font.page(code) {
                 None => continue,
                 Some(texture) => render.set_texture(texture),
             }
 
-            render.draw_rect_accept(UsedShader::Font, char, Some(st));
+            let char_rect = self.font.char_rect(n as u32, pos);
+            let st_rect = self.font.st_rect(code);
+
+            render.draw_rect_accept(UsedShader::Font, char_rect, Some(st_rect));
         }
     }
 
-    pub fn text_size(&self, text: &str) -> Vec2D<u32> {
-        match text.chars().count() {
-            0 => Vec2D::new(0, self.char_size.y),
-            1 => self.char_size,
-            l => Vec2D::new(
-                self.char_size.x + (l as u32 - 1) * (self.font.indent as u32 + self.char_size.x),
-                self.char_size.y,
-            ),
-        }
-    }
+    pub fn text_size(&self, text: &str) -> Vec2D<u32> { self.font.text_size(text) }
 }
